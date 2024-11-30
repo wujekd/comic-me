1482a793627f1420af6c45f38172a8fcb6248a58
Dominik Wujek <dom.wujek@gmail.com>
2024-11-30 13:00:02 +0000

    add story component

--- components/AddStory.js
+++ components/AddStory.js
@@ -9,14 +9,33 @@ export class AddStory extends HTMLElement {
         const temp = document.getElementById('add-template');
         const node = temp.content.cloneNode(true);
         this.appendChild(node);
-        this.render();
 
+        const fileInput = document.getElementById("fileInput");
+        fileInput.addEventListener("change", (e) => {
+            const file = e.target.files[0]; // Get the selected file
+            if (file) {
+                // Display it
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    imagePlaceholder.textContent = ""; // Clear placeholder text
+                    const img = document.createElement("img");
+                    img.src = event.target.result;
+                    img.alt = "Selected Picture";
+                    img.style.maxWidth = "100%";
+                    img.style.maxHeight = "100%";
+                    imagePlaceholder.appendChild(img);
+                };
+                reader.readAsDataURL(file);
+            } else {
+                imagePlaceholder.textContent = "Select a file"; 
+            }
+        });
     }
 
     
 }
 
 
-customElements.define("add-story", AddStory);
+customElements.define("add-page", AddStory);
 
 
